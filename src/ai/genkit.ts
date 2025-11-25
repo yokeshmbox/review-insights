4dc0939a3954761a1a630fdff74fff605fdfbb8d
yokeshmbox <yokeshmbox@gmail.com>
2025-11-25 13:35:28 +0530

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,9 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: "AIzaSyALXFB3NRuEcPxYqkfbrJ6UVEnCY0m4Img",
+      // apiKey: "AIzaSyALXFB3NRuEcPxYqkfbrJ6UVEnCY0m4Img",
+      // apiKey: "AIzaSyBE-ylW8NcRr-dsGqUG3XZlHrMLiMyxcHc",
+      apiKey: "AIzaSyClLC1ec_MU7X2xira4DxzIcBbDREcpJwk",
     }),
   ],
   logLevel: 'debug',
